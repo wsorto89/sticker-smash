2d60e828f27d7cab836bbb35ee8f21840d3fb019
Walter Sorto <wsorto89@gmail.com>
2025-01-28 00:58:30 -0800

    first commit

---
+++ app.config.js
@@ -0,0 +1,79 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return "com.wsorto89.stickersmash.dev";
+  }
+
+  if (IS_PREVIEW) {
+    return "com.wsorto89.stickersmash.preview";
+  }
+
+  return "com.wsorto89.stickersmash";
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return "StickerSmash (Dev)";
+  }
+
+  if (IS_PREVIEW) {
+    return "StickerSmash (Preview)";
+  }
+
+  return "StickerSmash: Emoji Stickers";
+};
+
+export default {
+  expo: {
+    name: getAppName(),
+    slug: "StickerSmash",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: getUniqueIdentifier(),
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "b6f08fdf-87c9-405d-91f2-ccbb10d8338e",
+      },
+    },
+    owner: "wsorto89",
+  },
+};
---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
+}
